e5745d140779c509cac1906676bcd67c3c4d9662
T0ny <62897985+4ntonioSouza@users.noreply.github.com>
2021-06-06 16:59:44 -0300

    Generating weights and bias.

--- src/br.com.antoniosouza/NeuralNetwork.java
+++ src/br.com.antoniosouza/NeuralNetwork.java
@@ -1,3 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class NeuralNetwork {
     
+    private Random random = new Random();
+    private int nInputs, nHidden, nOutputs;
+    private List<Double> hiddenLayer = new ArrayList<Double>();
+    private List<Double> outputLayer = new ArrayList<Double>();
+
+    /*
+    * @param nInputs - Number of input neurons
+    * @param nHidden - Number of hidden neurons
+    * @param nOutputs - Number of output neurons
+    */
+    public NeuralNetwork (int nInputs, int nHidden, int nOutputs) {
+        this.nInputs = nInputs;
+        this.nHidden = nHidden;
+        this.nOutputs = nOutputs;
+
+        for (int i = 0; i < nHidden + 1; i++) {
+            hiddenLayer.add(-1 + (1 + 1) * random.nextDouble()); // generate a random number between -1 and 1.
+        }
+        for (int i = 0; i < nOutputs + 1; i++) {
+            outputLayer.add(-1 + (1 + 1) * random.nextDouble()); // generate a random number between -1 and 1.
+        }
+    }
 }
