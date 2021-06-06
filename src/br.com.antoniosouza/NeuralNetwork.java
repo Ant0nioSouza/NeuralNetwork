da2ed2e63f3fd7705bd70d3f420e72b0f242538a
T0ny <62897985+4ntonioSouza@users.noreply.github.com>
2021-06-06 17:59:54 -0300

    Input from input layer

--- src/br.com.antoniosouza/NeuralNetwork.java
+++ src/br.com.antoniosouza/NeuralNetwork.java
@@ -8,17 +8,18 @@ public class NeuralNetwork {
     private int nInputs, nHidden, nOutputs;
     private List<Double> hiddenLayer = new ArrayList<Double>();
     private List<Double> outputLayer = new ArrayList<Double>();
+    private List<Double> inputLayer;
 
     /*
     * @param nInputs - Number of input neurons
     * @param nHidden - Number of hidden neurons
     * @param nOutputs - Number of output neurons
     */
-    public NeuralNetwork (int nInputs, int nHidden, int nOutputs) {
+    public NeuralNetwork (int nInputs, int nHidden, int nOutputs, List<Double> input) {
         this.nInputs = nInputs;
         this.nHidden = nHidden;
         this.nOutputs = nOutputs;
-
+        inputLayer = input;
         for (int i = 0; i < nHidden + 1; i++) {
             hiddenLayer.add(-1 + (1 + 1) * random.nextDouble()); // generate a random number between -1 and 1.
         }
@@ -29,5 +30,9 @@ public class NeuralNetwork {
 
     private Double neuronActivation (double weigth, double input, double bias) {
         return (weigth * input) + bias;
+    }
+
+    private Double neuronTransfer (double activation) {
+        return 1.0 / (1.0 + Math.exp(-activation));
     }
 }
