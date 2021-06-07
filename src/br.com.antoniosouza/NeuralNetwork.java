6450086babcca271da4ae2e0b6d1475304058d8f
T0ny <62897985+4ntonioSouza@users.noreply.github.com>
2021-06-07 18:16:39 -0300

    Transformim layers from list to 2D array.

--- src/br.com.antoniosouza/NeuralNetwork.java
+++ src/br.com.antoniosouza/NeuralNetwork.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
@@ -9,10 +8,10 @@ public class NeuralNetwork {
     private int nInputs, nHidden, nOutputs;
     private double hiddenBias, outputBias;
     private List<Double> newInputs = new ArrayList<Double>();
-    private List<Double> hiddenLayer = new ArrayList<Double>();
-    private List<Double> outputLayer = new ArrayList<Double>();
+    private Double[][] hiddenLayer;
+    private Double[][] outputLayer;
     private List<Double> inputLayer;
-    private List[] network = new List[2];
+    private Double[][][] network = new Double[2][0][0];
 
     /*
     * @param nInputs - Number of input neurons
@@ -24,14 +23,19 @@ public class NeuralNetwork {
         this.nHidden = nHidden;
         this.nOutputs = nOutputs;
         inputLayer = input;
+        hiddenLayer = new Double[nHidden][4];
+        outputLayer = new Double[nOutputs][4];
+
+
         for (int i = 0; i < nHidden + 1; i++) {
-            hiddenLayer.add(-1 + (1 + 1) * random.nextDouble()); // generate a random number between -1 and 1.
+            hiddenLayer[i][0] = (-1 + (1 + 1) * random.nextDouble()); // generate a random number between -1 and 1.
         }
         for (int i = 0; i < nOutputs + 1; i++) {
-            outputLayer.add(-1 + (1 + 1) * random.nextDouble()); // generate a random number between -1 and 1.
+            outputLayer[i][0] = (-1 + (1 + 1) * random.nextDouble()); // generate a random number between -1 and 1.
         }
-        hiddenBias = hiddenLayer.get(nHidden);
-        outputBias = outputLayer.get(nOutputs);
+
+        hiddenBias = hiddenLayer[nHidden][3];
+        outputBias = outputLayer[nOutputs][3];
         network[0] = hiddenLayer;
         network[1] = outputLayer;
     }
@@ -46,16 +50,15 @@ public class NeuralNetwork {
 
     private void forwardPropagation() {
         
-        for (List<Double> layer : network) {
-            System.out.println("TAMANHO DA LAYER E INPUT: " + layer.size() + "  " + inputLayer.size());
-            for (int i = 0; i < layer.size() - 1; i++) {
-                if (layer == network[0]) {
+        for (Double[][] layer : network) {
+            for (int i = 0; i < layer.length - 1; i++) {
+                if (layer == network[1]) {
                     for (int j = 0; j < inputLayer.size(); j++) {
-                        newInputs.add(neuronTransfer(neuronActivation(layer.get(i), inputLayer.get(j), hiddenBias)));
+                        newInputs.add(neuronTransfer(neuronActivation(layer[i][0], inputLayer.get(j), hiddenBias)));
                     }
-                } else if (layer == network[1]) {
+                } else if (layer == network[0]) {
                     for (int j = 0; j < inputLayer.size(); j++) {
-                        newInputs.add(neuronTransfer(neuronActivation(layer.get(i), inputLayer.get(j), outputBias)));
+                        newInputs.add(neuronTransfer(neuronActivation(layer[i][0], inputLayer.get(j), outputBias)));
                     }
                 }
             }
