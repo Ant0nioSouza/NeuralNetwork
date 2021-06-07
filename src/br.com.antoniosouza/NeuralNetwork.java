ce7e41404a4408bf70397e381b5a8615f289fa58
T0ny <62897985+4ntonioSouza@users.noreply.github.com>
2021-06-07 15:03:12 -0300

    added feed forward propagation and bias variable

--- src/br.com.antoniosouza/NeuralNetwork.java
+++ src/br.com.antoniosouza/NeuralNetwork.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
@@ -6,9 +7,12 @@ public class NeuralNetwork {
     
     private Random random = new Random();
     private int nInputs, nHidden, nOutputs;
+    private double hiddenBias, outputBias;
+    private List<Double> newInputs = new ArrayList<Double>();
     private List<Double> hiddenLayer = new ArrayList<Double>();
     private List<Double> outputLayer = new ArrayList<Double>();
     private List<Double> inputLayer;
+    private List[] network = new List[2];
 
     /*
     * @param nInputs - Number of input neurons
@@ -26,6 +30,10 @@ public class NeuralNetwork {
         for (int i = 0; i < nOutputs + 1; i++) {
             outputLayer.add(-1 + (1 + 1) * random.nextDouble()); // generate a random number between -1 and 1.
         }
+        hiddenBias = hiddenLayer.get(nHidden);
+        outputBias = outputLayer.get(nOutputs);
+        network[0] = hiddenLayer;
+        network[1] = outputLayer;
     }
 
     private Double neuronActivation (double weigth, double input, double bias) {
@@ -34,5 +42,24 @@ public class NeuralNetwork {
 
     private Double neuronTransfer (double activation) {
         return 1.0 / (1.0 + Math.exp(-activation));
+    }
+
+    private void forwardPropagation() {
+        
+        for (List<Double> layer : network) {
+            System.out.println("TAMANHO DA LAYER E INPUT: " + layer.size() + "  " + inputLayer.size());
+            for (int i = 0; i < layer.size() - 1; i++) {
+                if (layer == network[0]) {
+                    for (int j = 0; j < inputLayer.size(); j++) {
+                        newInputs.add(neuronTransfer(neuronActivation(layer.get(i), inputLayer.get(j), hiddenBias)));
+                    }
+                } else if (layer == network[1]) {
+                    for (int j = 0; j < inputLayer.size(); j++) {
+                        newInputs.add(neuronTransfer(neuronActivation(layer.get(i), inputLayer.get(j), outputBias)));
+                    }
+                }
+            }
+            inputLayer = newInputs;
+        }
     }
 }
